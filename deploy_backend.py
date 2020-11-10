b1da645ba9b5184fa3d46eeafcb552b2ede25a69
Miguel Jimenez <miguel1315@gmail.com>
2020-11-10 21:50:18 +0100

    Add deploy scripts to deploy backend and frontend

---
+++ deploy_backend.py
@@ -0,0 +1,29 @@
+#! /usr/bin/python
+import os
+import sys
+from pypendency.builder import container_builder
+from pypendency.loaders.yaml_loader import YamlLoader
+
+
+if len(sys.argv) != 3:
+    print("Incorrect Number of Parameters Error")
+    sys.exit(1)
+
+if not(sys.argv[1] == "PRO" or sys.argv[1] == "PRE"):
+    print("Environment Argument Error")
+    sys.exit(1)
+
+if not os.path.exists(sys.argv[2]):
+    print("Path Argument Error")
+    sys.exit(1)
+
+environment = sys.argv[1]
+path = sys.argv[2]
+
+YamlLoader(container_builder).load_dir('{}/use_cases/_dependencies/'.format(os.getcwd()))
+
+use_case = container_builder.get(
+    "use_cases.deploy_serverless_code_use_case.DeployServerlessCodeUseCase"
+)
+
+use_case.deploy(environment, path)
