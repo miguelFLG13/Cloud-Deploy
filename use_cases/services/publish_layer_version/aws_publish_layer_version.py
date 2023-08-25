cdbfd414f654aa79047b2e4e26eed39ae9e83ff1
Miguel Jimenez <miguel1315@gmail.com>
2023-08-25 12:25:02 +0200

    Add serverless config deploy and layers by environment

--- use_cases/services/publish_layer_version/aws_publish_layer_version.py
+++ use_cases/services/publish_layer_version/aws_publish_layer_version.py
@@ -5,38 +5,33 @@ from time import strftime
 from entities.artifact import Artifact
 from entities.bucket import Bucket
 from entities.layer import Layer
-from use_cases.services.publish_layer_version.publish_layer_version import PublishLayerVersion
+from use_cases.services.publish_layer_version.publish_layer_version import (
+    PublishLayerVersion,
+)
 
 
 class AwsPublishLayerVersion(PublishLayerVersion):
     STATUS_CODE_OK = 201
 
-    def publish(
-        self,
-        bucket: Bucket,
-        artifact: Artifact
-    ) -> Layer:
+    def publish(self, bucket: Bucket, artifact: Artifact) -> Layer:
         client = boto3.client(
-            'lambda',
-            aws_access_key_id=os.getenv('ACCESS_KEY_ID'),
-            aws_secret_access_key=os.getenv('SECRET_ACCESS_KEY'),
-            region_name=os.getenv('REGION_NAME')
+            "lambda",
+            aws_access_key_id=os.getenv("ACCESS_KEY_ID"),
+            aws_secret_access_key=os.getenv("SECRET_ACCESS_KEY"),
+            region_name=os.getenv("REGION_NAME"),
         )
 
         version = strftime("%Y%m%d%H%M%S")
         response = client.publish_layer_version(
-            LayerName=os.getenv('LAYER_NAME'),
+            LayerName=f"{bucket.environment}-{os.environ['LAYER_NAME']}",
             Description=version,
             Content={
-                'S3Bucket': bucket.name,
-                'S3Key': artifact.file_name,
+                "S3Bucket": bucket.name,
+                "S3Key": artifact.file_name,
             },
-            CompatibleRuntimes=[
-                'python3.8'
-            ]
         )
 
-        if response['ResponseMetadata']['HTTPStatusCode'] != self.STATUS_CODE_OK:
-            raise Exception('Error publishing layer:\n{}'.format(str(response)))
+        if response["ResponseMetadata"]["HTTPStatusCode"] != self.STATUS_CODE_OK:
+            raise Exception("Error publishing layer:\n{}".format(str(response)))
 
-        return response['LayerVersionArn']
+        return response["LayerVersionArn"]
